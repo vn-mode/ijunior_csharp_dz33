ac286976b9278d829062c86d650a97f1474d18fd
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-02-28 22:10:31 +0300

    ijunior_csharp_dz33

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Dictionary<string, string> dictionary = new Dictionary<string, string>();
+dictionary.Add("яблоко", "это фрукт");
+dictionary.Add("кошка", "это животное");
+findTheMeaningWord(dictionary);
+
+static void findTheMeaningWord(Dictionary<string, string> dictionary)
+{
+    string userInput = Console.ReadLine().ToLower();
+
+    if (dictionary.ContainsKey(userInput))
+        Console.WriteLine(dictionary[userInput]);
+    else
+        Console.WriteLine("По запросу ничего не найдено.");
+}
